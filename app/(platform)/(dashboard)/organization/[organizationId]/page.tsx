301ac2c582c182a32c9fce3d8ca93f76bafcd019
Samarth <shrivastavasamarth22@gmail.com>
2023-12-21 21:55:17 +0530

    Made a db file and successfully tested server action of Prisma

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,30 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
+
+	async function create(formData: FormData) {
+		"use server";
+
+		const title = formData.get("title") as string;
+
+		await db.board.create({
+			data: {
+				title
+			}
+		})
+	}
+
 	return (
 		<div>
-			<h1>Organization page</h1>
+			<form action={create}>
+				<input
+					id="title"
+					name="title"
+					required
+					placeholder="Enter a board title"
+					className="border border-black p-1"
+				/>
+			</form>
 		</div>
 	);
 };
