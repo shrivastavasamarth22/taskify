92563686a534a765f8e6ba427f2033b76612d690
Samarth <shrivastavasamarth22@gmail.com>
2023-12-21 22:07:22 +0530

    Created create-board server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { create } from "@/actions/create-board";
 import { db } from "@/lib/db";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
 
-	async function create(formData: FormData) {
-		"use server";
-
-		const title = formData.get("title") as string;
-
-		await db.board.create({
-			data: {
-				title
-			}
-		})
-	}
+	const boards = await db.board.findMany();
 
 	return (
-		<div>
+		<div className="flex flex-col space-y-4">
 			<form action={create}>
 				<input
 					id="title"
@@ -24,7 +16,17 @@ const OrganizationIdPage = () => {
 					placeholder="Enter a board title"
 					className="border border-black p-1"
 				/>
+				<Button type="submit">
+					Submit
+				</Button>
 			</form>
+			<div className="space-y-2">
+				{boards.map(board => (
+					<div key={board.id}>
+						Board Name: {board.title}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
