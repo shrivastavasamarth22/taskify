615cfc95a34095a967fa147b58303516f545cb84
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-12-25 18:40:08 +0530

    added new wrapper function for server actions called createSafeAction

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,49 +1,49 @@
-"use server"
+"use server";
 
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
-import {z} from "zod";
+import { z } from "zod";
 
 export type State = {
-    errors?: {
-        title?: string
-    },
-    message?: string | null
-}
+	errors?: {
+		title?: string[] | undefined;
+	};
+	message?: string | null;
+};
 
 const CreateBoard = z.object({
-    title: z.string().min(3, {
-        message: "Minimum length of 3 characters is required"
-    })
-})
+	title: z.string().min(3, {
+		message: "Minimum length of 3 characters is required",
+	}),
+});
 
 export async function create(prevState: State, formData: FormData) {
-    const validatedFields = CreateBoard.safeParse({
-        title: formData.get("title")
-    })
+	const validatedFields = CreateBoard.safeParse({
+		title: formData.get("title"),
+	});
 
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: "Missing fields"
-        }
-    }
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing fields",
+		};
+	}
 
-    const { title } = validatedFields.data
+	const { title } = validatedFields.data;
 
-    try {
-        await db.board.create({
-            data: {
-                title
-            }
-        })
-    } catch (error) {
-        return {
-            message: "Database error"
-        }
-    }
+	try {
+		await db.board.create({
+			data: {
+				title,
+			},
+		});
+	} catch (error) {
+		return {
+			message: "Database error",
+		};
+	}
 
-    revalidatePath("/organization/org_2Zo8DmTzEDwvkmJ83ZacU6IfYce")
-    redirect("/organization/org_2Zo8DmTzEDwvkmJ83ZacU6IfYce")
+	revalidatePath("/organization/org_2Zo8DmTzEDwvkmJ83ZacU6IfYce");
+	redirect("/organization/org_2Zo8DmTzEDwvkmJ83ZacU6IfYce");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,23 +1,11 @@
-import { Board } from "./board";
-import { Form } from "./form";
 import { db } from "@/lib/db";
 
 const OrganizationIdPage = async () => {
-
 	const boards = await db.board.findMany();
 
 	return (
 		<div className="flex flex-col space-y-4">
-			<Form />
-			<div className="space-y-2">
-				{boards.map(board => (
-					<Board
-						key={board.id}
-						id={board.id}
-						title={board.title}
-					/>
-				))}
-			</div>
+			<h1>Hello</h1>
 		</div>
 	);
 };
