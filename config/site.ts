bfa808567a4b3e49afc2a81b7207b9a1eecc33e5
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-12-27 12:49:53 +0530

    Added dynamic title functionality to  org page

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-	name: "taskify",
+	name: "Taskify",
 	description:
 		"Collaborate, manage projects and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Taskify.",
 };
