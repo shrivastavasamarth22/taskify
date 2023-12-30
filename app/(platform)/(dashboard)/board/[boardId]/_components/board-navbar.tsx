94a93cd6be700e4d8f5d049462f323eefcd91d5c
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-12-30 10:44:42 +0530

    made board-options component for board-navbar

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,6 @@
 import { Board } from "@prisma/client";
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
 interface BoardNavbarProps {
     data: Board
@@ -16,6 +17,11 @@ export const BoardNavbar = async ({
             <BoardTitleForm 
                 data={data}
             />
+            <div className="ml-auto">
+                <BoardOptions 
+                    id={data.id}
+                />
+            </div>
         </div>
     )
 }
