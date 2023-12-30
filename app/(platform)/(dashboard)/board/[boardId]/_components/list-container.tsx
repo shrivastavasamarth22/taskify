7a7e66135962eb76476cbb7cc756c82b6dce10d9
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2023-12-30 23:42:51 +0530

    created list item and list header components

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,8 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
     boardId: string;
@@ -12,8 +13,25 @@ export const ListContainer = ({
     boardId,
     data
 }: ListContainerProps) => {
+    // used to order the lists locally and then send the order to the server for a good UX
+    const [orderedData, setOrderedData] = useState(data);
+
+    // update the orderedData when the data changes
+    useEffect(() => {
+        setOrderedData(data)
+    }, [data])
+
     return (
-        <ol>
+        <ol
+            className="flex gap-x-3 h-full"
+        >
+            {orderedData.map((list, index) => (
+                <ListItem 
+                    key={list.id}
+                    index={index}
+                    data={list}
+                />
+            ))}
             <ListForm />
             <div 
                 className="flex-shrink-0 w-1"
