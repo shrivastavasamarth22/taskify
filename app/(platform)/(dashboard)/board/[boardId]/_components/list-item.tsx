b6df1a324f8003a9a6bdbc9d58c8b41f66770d7c
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-01-02 13:28:00 +0530

    created a new component called card-item

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { ElementRef, useRef, useState } from "react";
+import { cn } from "@/lib/utils";
+
 import { ListWithCards } from "@/types";
 
 import { ListHeader } from "./list-header";
 import { CardForm } from "./card-form";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
     index: number;
@@ -37,6 +40,22 @@ export const ListItem = ({
                     onAddCard={enableEditing}
                     data={data}
                 />
+                <ol
+                    className={cn(
+                        "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+                        data.cards.length > 0 ? "mt-2" : "mt-0"
+                    )}
+                >
+                    {
+                        data.cards.map((card, index) => (
+                            <CardItem 
+                                key={card.id}
+                                index={index}
+                                data={card}
+                            />
+                        ))
+                    }
+                </ol>
                 <CardForm 
                     ref={textareaRef}
                     listId={data.id}
