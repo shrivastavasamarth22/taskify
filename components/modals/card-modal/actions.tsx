a1828f55874b55f9aaf30339d68fd72625d334d5
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-01-06 14:36:15 +0530

    made a change to base files

---
+++ components/modals/card-modal/actions.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Actions = () => {
+    return (
+        <div>
+            <h1>Actions</h1>
+        </div>
+    );
+};
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -8,6 +8,7 @@ import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
     const id = useCardModal((state) => state.id);
@@ -33,6 +34,7 @@ export const CardModal = () => {
                             )}
                         </div>
                     </div>
+                    <Actions />
                 </div>
             </DialogContent>
         </Dialog>
