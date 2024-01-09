2570055859b9657459a2c03b03efd3aedab1e2c6
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-01-09 13:54:19 +0530

    successfully added activity item to activity component of card modal

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ActivityItem } from "@/components/activity-item";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuditLog } from "@prisma/client";
+import { ActivityIcon } from "lucide-react";
 
 interface AcivityProps {
     items: AuditLog[];
@@ -9,8 +11,16 @@ interface AcivityProps {
 
 export const Activity = ({ items }: AcivityProps) => {
     return (
-        <div>
-            <h1>Activity</h1>
+        <div className="flex items-start gap-x-3 w-full">
+            <ActivityIcon className="h-5 w-5 mt-0.5 text-neutral-700" />
+            <div className="w-full">
+                <p className="font-semibold text-neutral-700 mb-2">Activity</p>
+                <ol className="mt-2 space-y-4">
+                    {items.map((item) => (
+                        <ActivityItem key={item.id} data={item} />
+                    ))}
+                </ol>
+            </div>
         </div>
     );
 };
